f9828245f784e894ba9442f7179dbcf9afcad2c7
itning <itning@itning.top>
2018-12-20 14:36:35 +0800

    init

---
+++ src/main/java/top/yunshu/shw_server/ShwServerApplication.java
@@ -0,0 +1,14 @@
+package top.yunshu.shw_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShwServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShwServerApplication.class, args);
+    }
+
+}
+
