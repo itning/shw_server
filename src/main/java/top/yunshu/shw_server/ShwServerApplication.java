c84b0e3e8432448bbab4ac817cda55c96f92f8f1
itning <itning@itning.top>
2018-12-21 14:16:09 +0800

    init frame

--- src/main/java/top/yunshu/shw_server/ShwServerApplication.java
+++ src/main/java/top/yunshu/shw_server/ShwServerApplication.java
@@ -3,6 +3,12 @@ package top.yunshu.shw_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 学生作业管理系统
+ *
+ * @author itning
+ * @date 2018/12/21
+ */
 @SpringBootApplication
 public class ShwServerApplication {
 
---
+++ src/main/java/top/yunshu/shw_server/controller/teacher/TeacherController.java
@@ -0,0 +1,16 @@
+package top.yunshu.shw_server.controller.teacher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 教师控制器
+ *
+ * @author itning
+ * @date 2018/12/21
+ */
+@RestController
+public class TeacherController {
+    private static final Logger logger = LoggerFactory.getLogger(TeacherController.class);
+}
