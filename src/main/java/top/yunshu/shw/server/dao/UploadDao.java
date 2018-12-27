ee8a8fc58650493c5caba376ed1635fe9db6f840
itshulu <1157830249@qq.com>
2018-12-27 18:36:10 +0800

    Merge branch 'master' of C:\Users\舒露\Desktop\shw_server with conflicts.

--- src/main/java/top/yunshu/shw/server/dao/UploadDao.java
+++ src/main/java/top/yunshu/shw/server/dao/UploadDao.java
@@ -14,10 +14,10 @@ public interface UploadDao extends JpaRepository<Upload, UploadPrimaryKey> {
      * 根据作业ID查询上传是否存在
      *
      * @param studentId 学号
-     * @param workId    作业ID
+     * @param workId 作业ID
      * @return 存在返回<code>true</code>
      */
-    boolean existsByWorkIdAndWorkId(String studentId, String workId);
+    boolean existsByWorkIdAndStudentId(String studentId,String workId);
 
     /**
      * 根据学生学号和作业ID获取上传信息
