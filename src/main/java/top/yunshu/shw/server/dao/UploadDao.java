fcd1c8cc7b40c6f97a5e655ec104040efbddd1b1
itshulu <1157830249@qq.com>
2018-12-27 18:23:23 +0800

    update

--- src/main/java/top/yunshu/shw/server/dao/UploadDao.java
+++ src/main/java/top/yunshu/shw/server/dao/UploadDao.java
@@ -13,10 +13,11 @@ public interface UploadDao extends JpaRepository<Upload, UploadPrimaryKey> {
     /**
      * 根据作业ID查询上传是否存在
      *
+     * @param studentId 学号
      * @param workId 作业ID
      * @return 存在返回<code>true</code>
      */
-    boolean existsByWorkId(String workId);
+    boolean existsByWorkId(String studentId,String workId);
 
     /**
      * 根据学生学号和作业ID获取上传信息
