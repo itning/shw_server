4f85ed59ff29e87423381dd6daff5a94c2cd5c11
itning <itning@itning.top>
2019-04-30 11:48:59 +0800

    add shw-security server

--- shw-security/src/main/java/top/itning/server/shwsecurity/ShwSecurityApplication.java
+++ shw-security/src/main/java/top/itning/server/shwsecurity/ShwSecurityApplication.java
@@ -1,9 +1,7 @@
 package top.itning.server.shwsecurity;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
