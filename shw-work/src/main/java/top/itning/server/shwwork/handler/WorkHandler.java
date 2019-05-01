af8ef237ccbee2eaff85dcd633d0db56f40eb789
itning <itning@itning.top>
2019-05-01 13:36:32 +0800

    handler and exception

--- shw-work/src/main/java/top/itning/server/shwwork/handler/WorkHandler.java
+++ shw-work/src/main/java/top/itning/server/shwwork/handler/WorkHandler.java
@@ -1,8 +1,45 @@
 package top.itning.server.shwwork.handler;
 
+import org.apache.commons.lang.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+import top.itning.server.shwwork.service.WorkService;
+
+import static top.itning.server.common.model.RestModel.ok;
+import static top.itning.server.common.util.Preconditions.getNo;
+import static top.itning.server.common.util.Preconditions.mustStudentLogin;
+
 /**
  * @author itning
  * @date 2019/5/1 9:42
  */
+@Component
 public class WorkHandler {
+    private final WorkService workService;
+
+    @Autowired
+    public WorkHandler(WorkService workService) {
+        this.workService = workService;
+    }
+
+    @NonNull
+    public Mono<ServerResponse> getAllUnDoneWorks(ServerRequest request) {
+        mustStudentLogin(request);
+        int page = NumberUtils.toInt(request.queryParam("page").orElse("0"));
+        int size = NumberUtils.toInt(request.queryParam("size").orElse("20"));
+        return ok(workService.getStudentUnDoneWork(getNo(request), page, size));
+
+    }
+
+    @NonNull
+    public Mono<ServerResponse> getAllDoneWorks(ServerRequest request) {
+        mustStudentLogin(request);
+        int page = NumberUtils.toInt(request.queryParam("page").orElse("0"));
+        int size = NumberUtils.toInt(request.queryParam("size").orElse("20"));
+        return ok(workService.getStudentDoneWork(getNo(request), page, size));
+    }
 }
--- shw-work/src/main/java/top/itning/server/shwwork/router/WorkRouters.java
+++ shw-work/src/main/java/top/itning/server/shwwork/router/WorkRouters.java
@@ -1,8 +1,29 @@
 package top.itning.server.shwwork.router;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import top.itning.server.shwwork.handler.WorkHandler;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.all;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
 /**
  * @author itning
  * @date 2019/5/1 9:42
  */
+@Configuration
 public class WorkRouters {
+    @Bean
+    RouterFunction<ServerResponse> userRouter(WorkHandler workHandler) {
+        return nest(
+                all(),
+                route()
+                        .GET("/un_done", workHandler::getAllUnDoneWorks)
+                        .GET("/done", workHandler::getAllDoneWorks)
+                        .build()
+        );
+    }
 }
