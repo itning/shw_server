7513b8e7a5044af0c8668837016aa3231533d440
itning <itning@itning.top>
2020-05-16 11:25:32 +0800

    chore: remove string

--- install-aspose-dependency.bat
+++ install-aspose-dependency.bat
@@ -2,7 +2,7 @@
 echo author itning
 set ROOT_DIR=%~dp0
 set JAR_DIR=%ROOT_DIR%pic\
-set INSTALL_VERSION=1.0.0.RELEASE
+set INSTALL_VERSION=1.0.0
 if not exist %JAR_DIR%aspose-cells-8.6.2.jar call:file_not_found_func aspose-cells-8.6.2.jar
 if not exist %JAR_DIR%aspose-slides-15.7.0-edu.jar call:file_not_found_func aspose-slides-15.7.0-edu.jar
 if not exist %JAR_DIR%aspose-words-15.8.0-jdk16.jar call:file_not_found_func aspose-words-15.8.0-jdk16.jar
